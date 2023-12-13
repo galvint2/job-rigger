175a1de290e49520624f36ac35fa94953fb025ce
Thomas Galvin <galvint2@gmail.com>
2023-12-13 14:47:01 -0700

    init project

---
+++ job_rigger/__init__.py
@@ -0,0 +1,1 @@
+# This is an empty __init__.py file that declares this directory as a Python package.
---
+++ job_rigger/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to job_rigger!")
+
+
+if __name__ == "__main__":
+    main()
