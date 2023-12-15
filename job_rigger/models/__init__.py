79d786e89094c3112f76a9abef7dc9efed045dd7
Thomas Galvin <galvint2@gmail.com>
2023-12-15 11:28:36 -0700

    update ruff linting rules

---
+++ job_rigger/models/__init__.py
@@ -0,0 +1,2 @@
+from job_rigger.models.job_detail import JobDetail
+from job_rigger.models.job_search_result import JobSearchResult, JobSearchResultList
--- job_rigger/models/job_search_result.py
+++ job_rigger/models/job_search_result.py
@@ -7,12 +7,9 @@ class JobSearchResult(BaseModel):
     model_config = ConfigDict(
         populate_by_name=True,
     )
-    tracking_urn: str = Field(..., alias="trackingUrn")
     reposted_job: bool = Field(..., alias="repostedJob")
     title: str
-    recipe_types: list[str] = Field(..., alias="$recipeTypes")
     poster_id: str = Field(..., alias="posterId")
-    type: str = Field(..., alias="$type")
     content_source: str = Field(..., alias="contentSource")
     entity_urn: str = Field(..., alias="entityUrn")
 
