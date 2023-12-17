5fa3ee76fe58f83452d30fb6045a1acaf446b348
Thomas Galvin <galvint2@gmail.com>
2023-12-16 19:57:43 -0800

    rename company -> company_detail

--- job_rigger/models/__init__.py
+++ job_rigger/models/__init__.py
@@ -1,4 +1,3 @@
-from job_rigger.models.company import Company
 from job_rigger.models.job_detail import JobDetail, JobDetailList
 from job_rigger.models.job_search_result import JobSearchResult, JobSearchResultList
 from job_rigger.models.people_search_result import PeopleSearchResult, PeopleSearchResultList
