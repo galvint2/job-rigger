765d552cb38f67def95c34d60030accb14f04da8
Thomas Galvin <galvint2@gmail.com>
2023-12-16 19:58:11 -0800

    update imports

--- job_rigger/models/__init__.py
+++ job_rigger/models/__init__.py
@@ -1,3 +1,4 @@
+from job_rigger.models.company_detail import CompanyDetail, CompanyDetailList
 from job_rigger.models.job_detail import JobDetail, JobDetailList
 from job_rigger.models.job_search_result import JobSearchResult, JobSearchResultList
 from job_rigger.models.people_search_result import PeopleSearchResult, PeopleSearchResultList
