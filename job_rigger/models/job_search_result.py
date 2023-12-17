72a0407df5231edc30e991e61e56ca1a8b968b42
Thomas Galvin <galvint2@gmail.com>
2023-12-16 18:56:34 -0800

    update job search result / job search results models

--- job_rigger/models/job_search_result.py
+++ job_rigger/models/job_search_result.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, ConfigDict, Field, RootModel, field_validator
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 from job_rigger.utils import extract_entity_id
 
@@ -7,11 +9,11 @@ class JobSearchResult(BaseModel):
     model_config = ConfigDict(
         populate_by_name=True,
     )
-    reposted_job: bool = Field(..., alias="repostedJob")
     title: str
-    poster_id: str = Field(..., alias="posterId")
-    content_source: str = Field(..., alias="contentSource")
     entity_urn: str = Field(..., alias="entityUrn")
+    reposted_job: bool = Field(..., alias="repostedJob")
+    poster_id: Optional[str] = Field(..., alias="posterId")
+    content_source: str = Field(..., alias="contentSource")
 
     @field_validator("entity_urn")
     @classmethod
@@ -19,5 +21,8 @@ class JobSearchResult(BaseModel):
         return extract_entity_id(v)
 
 
-class JobSearchResultList(RootModel[list[JobSearchResult]]):
-    root: list[JobSearchResult]
+class JobSearchResults(BaseModel):
+    model_config = ConfigDict(
+        populate_by_name=True,
+    )
+    results: list[JobSearchResult]
